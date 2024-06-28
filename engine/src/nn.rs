662616bba532801da3d2af4c36853531669fdc24
mhonert <48153876+mhonert@users.noreply.github.com>
2024-06-28 16:26:22 +0200

    Add new neural network file trained using higher upper ReLU bound

--- engine/src/nn.rs
+++ engine/src/nn.rs
@@ -32,7 +32,7 @@ pub const INPUTS: usize = BUCKET_SIZE * BUCKETS;
 pub const HL1_NODES: usize = 2 * HL1_HALF_NODES;
 pub const HL1_HALF_NODES: usize = 768;
 
-pub const MAX_RELU: f32 = 1.999;
+pub const MAX_RELU: f32 = 2.499;
 pub const FP_MAX_RELU: i16 = (MAX_RELU * FP_IN_MULTIPLIER as f32) as i16;
 
 pub const INPUT_WEIGHT_COUNT: usize = INPUTS * HL1_HALF_NODES;
